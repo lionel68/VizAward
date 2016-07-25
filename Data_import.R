08bf7245b5c277727f95ed5d44ee03c30b046dec
nadjasimons <nadja@fam.hersacher.de>
2016-07-25 17:40:22 +0200

    Instructions on how to download map data for Hesse.

--- Data_import.R
+++ Data_import.R
@@ -33,7 +33,20 @@ csv_clean.spdf <- SpatialPointsDataFrame(csv_clean.df[,c("longitude_new","latitu
                                         data = csv_clean.df[c(5:12)])
 bbox(csv_clean.spdf)
 
+<<<<<<< HEAD
+GetMap.bbox(c(8.6,8.9),c(50.7,50.9)) -> marburg_region # Google Maps
+
+#  left, bottom, right, and top coordinates
+bbox_osm <- corner_bbox(8.6893,50.7700,8.8145,50.8437) # OpenStreetMap
+marburg_region_OSM <- get_osm(bbox_osm, source = osmsource_api()) # --> too large
+
+# manually downloaded a map of Hesse from http://download.geofabrik.de/europe/germany/hessen.html
+# and add to a new folder "VizAward_Data" which is on the same level as the current directory
+
+Hesse_roads <- readOGR("../VizAward_Data/hessen-latest.shp","roads")
+=======
 GetMap.bbox(c(8.6,8.9),c(50.7,50.9)) -> marburg_region
+>>>>>>> parent of 85880e9... Download of OpenStreetMap data for Hesse
 
 
 PlotOnStaticMap(marburg_region,lat=LatLon2XY(csv_clean.spdf@coords[,2],csv_clean.spdf@coords[,1],zoom=2)$Coords[,1],lon=LatLon2XY(csv_clean.spdf@coords[,2],csv_clean.spdf@coords[,1],zoom=2)$Coords[,2])
